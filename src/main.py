7e48e975761d98de50f110d511b6da3a966051c5
ThatGenZGamer48 <78960084+ThatGenZGamer48@users.noreply.github.com>
2021-11-17 12:49:05 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 with open("config.json", "r") as f:
     config_data = json.load(f)
-    
+
 GUILD_IDS = config_data["guild_ids"]
 TOKEN = config_data["token"]
 
@@ -18,17 +18,23 @@ class CalcView(discord.ui.View):
         self.values = ""
         super().__init__(timeout=None)
 
-    @discord.ui.button(label="Clear", style=discord.ButtonStyle.red, custom_id="calc_view:clear")
+    @discord.ui.button(
+        label="Clear", style=discord.ButtonStyle.red, custom_id="calc_view:clear"
+    )
     async def clear_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
         try:
             self.values = ""
-            await interaction.response.edit_message(content=f"You cleared the calculator! Start typing numbers or operations!")
+            await interaction.response.edit_message(
+                content=f"You cleared the calculator! Start typing numbers or operations!"
+            )
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="9", style=discord.ButtonStyle.green, custom_id="calc_view:nine")
+    @discord.ui.button(
+        label="9", style=discord.ButtonStyle.green, custom_id="calc_view:nine"
+    )
     async def nine_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -38,7 +44,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="8", style=discord.ButtonStyle.green, custom_id="calc_view:eight")
+    @discord.ui.button(
+        label="8", style=discord.ButtonStyle.green, custom_id="calc_view:eight"
+    )
     async def eight_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -48,7 +56,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="7", style=discord.ButtonStyle.green, custom_id="calc_view:seven")
+    @discord.ui.button(
+        label="7", style=discord.ButtonStyle.green, custom_id="calc_view:seven"
+    )
     async def seven_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -58,7 +68,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="=", style=discord.ButtonStyle.grey, custom_id="calc_view:equals")
+    @discord.ui.button(
+        label="=", style=discord.ButtonStyle.grey, custom_id="calc_view:equals"
+    )
     async def equals_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -67,7 +79,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("Please enter proper operations and values!")
 
-    @discord.ui.button(label="+", style=discord.ButtonStyle.grey, custom_id="calc_view:plus")
+    @discord.ui.button(
+        label="+", style=discord.ButtonStyle.grey, custom_id="calc_view:plus"
+    )
     async def plus_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -76,8 +90,10 @@ class CalcView(discord.ui.View):
             await interaction.response.edit_message(content=str(self.values))
         except:
             await interaction.channel.send("There was an error!")
-        
-    @discord.ui.button(label="6", style=discord.ButtonStyle.green, custom_id="calc_view:six")
+
+    @discord.ui.button(
+        label="6", style=discord.ButtonStyle.green, custom_id="calc_view:six"
+    )
     async def six_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -87,7 +103,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="5", style=discord.ButtonStyle.green, custom_id="calc_view:five")
+    @discord.ui.button(
+        label="5", style=discord.ButtonStyle.green, custom_id="calc_view:five"
+    )
     async def five_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -97,7 +115,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="4", style=discord.ButtonStyle.green, custom_id="calc_view:four")
+    @discord.ui.button(
+        label="4", style=discord.ButtonStyle.green, custom_id="calc_view:four"
+    )
     async def four_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -107,7 +127,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="-", style=discord.ButtonStyle.grey, custom_id="calc_view:minus")
+    @discord.ui.button(
+        label="-", style=discord.ButtonStyle.grey, custom_id="calc_view:minus"
+    )
     async def minus_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -117,7 +139,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="*", style=discord.ButtonStyle.grey, custom_id="calc_view:multiply")
+    @discord.ui.button(
+        label="*", style=discord.ButtonStyle.grey, custom_id="calc_view:multiply"
+    )
     async def multiply_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -127,7 +151,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="3", style=discord.ButtonStyle.green, custom_id="calc_view:three")
+    @discord.ui.button(
+        label="3", style=discord.ButtonStyle.green, custom_id="calc_view:three"
+    )
     async def three_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -137,7 +163,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="2", style=discord.ButtonStyle.green, custom_id="calc_view:two")
+    @discord.ui.button(
+        label="2", style=discord.ButtonStyle.green, custom_id="calc_view:two"
+    )
     async def two_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -147,7 +175,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="1", style=discord.ButtonStyle.green, custom_id="calc_view:one")
+    @discord.ui.button(
+        label="1", style=discord.ButtonStyle.green, custom_id="calc_view:one"
+    )
     async def one_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -157,7 +187,9 @@ class CalcView(discord.ui.View):
         except:
             await interaction.channel.send("There was an error!")
 
-    @discord.ui.button(label="/", style=discord.ButtonStyle.grey, custom_id="calc_view:divide")
+    @discord.ui.button(
+        label="/", style=discord.ButtonStyle.grey, custom_id="calc_view:divide"
+    )
     async def divide_callback(
         self, button: discord.ui.Button, interaction: discord.Interaction
     ):
@@ -186,9 +218,11 @@ class CalculatorBot(commands.Bot):
             self.add_view(CalcView())
             self.calc_view_added = True
 
-        print(f'{self.user} is ready! (ID: {self.user.id})')
+        print(f"{self.user} is ready! (ID: {self.user.id})")
+
 
 bot = CalculatorBot()
+
 
 @bot.slash_command(
     name="startcalc",
@@ -198,11 +232,13 @@ bot = CalculatorBot()
 async def _startcalc(ctx):
     await ctx.respond("Started calculator", view=CalcView())
 
+
 @bot.command(
     name="startcalc",
     description="Starts the dynamic calculator.",
 )
 async def _command_startcalc(ctx):
     await ctx.send("Started calculator", view=CalcView())
+
 
 bot.run(TOKEN)
